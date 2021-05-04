f61afc2e485d6c35a85ef8af87b82ab7cb5dc9c2
FelipePereiraDeGodoy <godoypereirafelipe@gmail.com>
2021-05-04 12:56:31 -0300

    realizado alteração e correção ao receber valores dos campos input

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -2,9 +2,9 @@ import react from 'react';
 
 const Input = ({ name, textLabel, placeholder, classDiv, classLabel, classInput, ...rest }) => {
     return (
-        <div className={classDiv} {...rest}>
+        <div className={classDiv} >
             <label className={classLabel} htmlFor={name}>{textLabel}</label>
-            <input className={classInput} type="text" name={name} placeholder={placeholder} />
+            <input className={classInput} type="text" name={name} placeholder={placeholder} {...rest} />
         </div>
     );
 }
--- src/pages/cadastroCliente/index.jsx
+++ src/pages/cadastroCliente/index.jsx
@@ -1,5 +1,6 @@
 import react, { useState } from 'react';
 import Input from '../../components/input/index';
+import api from '../../services/api';
 import './styles.css';
 
 const CadastroCliente = () => {
@@ -10,66 +11,107 @@ const CadastroCliente = () => {
     const [telefone1, setTelefone1] = useState("");
     const [dataNasc, setDataNasc] = useState();
 
+
+    const handleInsertCliente = (event) => {
+        event.preventDefault();
+
+        console.log(returnObjCliente());
+
+        //api.post('clientes', { returnObjCliente }).
+        //    then(() => {
+        //        alert("Cadastro realizado com sucesso!");
+        //        history.push('/');
+        //    }).
+        //    catch(() => {
+        //        alert("Erro ao tentar realizar cadastro!");
+        //    });
+    }
+
+
+    const returnObjCliente = () => {
+        const cliente = {
+            nome: nome,
+            cpf: cpf,
+            rg: rg,
+            email: email,
+            telefone1: telefone1,
+            dataNasc: dataNasc
+        };
+
+        return cliente;
+    }
+
     return (
         <div className="page-cadastro-cliente-block">
-            <form action="">
+            <form onSubmit={handleInsertCliente}>
 
                 <Input
+                    type="text"
                     name="nome"
                     textLabel="Nome:"
                     placeholder="Fulano de tal"
-                    id="input-nome"
                     classDiv="div-input-block"
                     classLabel="label-block"
                     classInput="input-block"
+                    onChange={(e) => { setNome(e.target.value) }}
                 />
 
                 <Input
+                    type="text"
                     name="cpf"
                     textLabel="CPF:"
                     placeholder="Fulano de tal"
                     classDiv="div-input-block"
                     classLabel="label-block"
                     classInput="input-block"
+                    onChange={(e) => { setCpf(e.target.value) }}
                 />
 
                 <Input
+                    type="text"
                     name="rg"
                     textLabel="RG:"
                     placeholder="Fulano de tal"
                     classDiv="div-input-block"
                     classLabel="label-block"
                     classInput="input-block"
+                    onChange={(e) => { setRg(e.target.value) }}
                 />
 
                 <Input
+                    type="email"
                     name="email"
                     textLabel="Email:"
                     placeholder="Fulano de tal"
                     classDiv="div-input-block"
                     classLabel="label-block"
                     classInput="input-block"
+                    onChange={(e) => { setEmail(e.target.value) }}
                 />
 
                 <Input
+                    type="text"
                     name="telefone1"
                     textLabel="Telefone1:"
                     placeholder="Fulano de tal"
                     classDiv="div-input-block"
                     classLabel="label-block"
                     classInput="input-block"
+                    onChange={(e) => { setTelefone1(e.target.value) }}
                 />
 
                 <Input
+                    type="date"
                     name="dataNasc"
                     textLabel="Data Nascimento:"
                     placeholder="Fulano de tal"
                     classDiv="div-input-block"
                     classLabel="label-block"
                     classInput="input-block"
+                    onChange={(e) => { setDataNasc(e.target.value) }}
                 />
 
-                <button className="btn-salvar">
+                <button className="btn-salvar" type="submit">
                     Salvar
             </button>
 
